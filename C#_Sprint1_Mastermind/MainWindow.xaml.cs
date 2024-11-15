5ecd223fd89ca88cd53d2778f77b1c2b766e7a67
Sajeel Ahmad <12301839@student.pxl.be>
2024-11-15 19:05:03 +0100

    MasterMind-02

--- C#_Sprint1_Mastermind/MainWindow.xaml.cs
+++ C#_Sprint1_Mastermind/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,11 +18,61 @@ namespace C__Sprint1_Mastermind
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+            GenerateCode();
         }
 
-        
+        private void GenerateCode()
+
+        {
+            Random code = new Random();
+            const int numberOfColorsCode = 4;
+            const int minNumber = 1;
+            const int maxNumber = 7;
+            int colorRed = 1;
+            int colorYellow = 2;
+            int colorOrange = 3;
+            int colorWhite = 4;
+            int colorGreen = 5;
+
+            for (int i = 1; i <= numberOfColorsCode; i++)
+            {
+                int colorCode = code.Next(minNumber, maxNumber);
+                if (colorCode == colorRed)
+                {
+                    this.Title += " Red";
+                }
+                else if (colorCode == colorYellow)
+                {
+                    this.Title += " Yellow";
+                }
+                else if (colorCode == colorOrange)
+                {
+                    this.Title += " Orange";
+                }
+                else if (colorCode == colorWhite)
+                {
+                    this.Title += " White";
+                }
+                else if (colorCode == colorGreen) 
+                {
+                    this.Title += " Green";
+                }
+                else
+                {
+                    this.Title += " Blue";
+                }
+            }
+        }
     }
-}
+} 
+        
+            
+        
+    
+
+
